34e185b92f5d936b093f41e7822faef89dbe85c3
Caleb Cheney <caleb_cheney@baylor.edu>
2022-03-23 19:55:18 -0500

    pc

--- Pizza.py
+++ Pizza.py
@@ -1,26 +1,30 @@
 import tkinter
+from tkinter import font
 import tkinter.messagebox
+
+
 
 class PizzaGUI:
     def __init__(self):
+        
         self.main_window = tkinter.Tk()
         self.main_window.geometry('600x300')
+        self.main_window['background']='#ebe6dd'
         self.main_window.title("Caleb's Pizzeria")
+        #self.font = tkinter.font.Font(family = 'Times', size = 20, weight = 'bold')
 
         self.first_frame = tkinter.Frame(self.main_window)
         self.second_frame = tkinter.Frame(self.main_window)
         self.third_frame = tkinter.Frame(self.main_window)
-        self.forth_frame = tkinter.Frame(self.main_window)
-        self.fifth_frame = tkinter.Frame(self.main_window)
+      
 
         #Order Name
         self.name = tkinter.Label(self.first_frame, text = "Enter an name for your order:")
-        def name(self):
-            self.entry_name = tkinter.Entry(self.first_frame, width=15)
-            return self.entry_name
+        self.entry_name = tkinter.Entry(self.first_frame, width=15)
+        
 
         #Toppings
-        self.toppings = tkinter.Label(self.second_frame, text = 'Toppings:')
+        self.toppings = tkinter.Label(self.second_frame, text = 'Toppings:', font = 'Times')
         
         self.cb_pep_var = tkinter.IntVar()
         self.cb_pineapple_var = tkinter.IntVar()
@@ -40,15 +44,29 @@ class PizzaGUI:
         self.cb_pineapple_box.pack()
         self.cb_onion_box.pack()
 
+        #crust
+        #radio buttons
+        self.crust = tkinter.Label(self.third_frame, text = 'Crust:', font='Times')
+        self.radio_var = tkinter.IntVar()
+
+        self.rb1 = tkinter.Radiobutton(self.third_frame, text = 'Thin              ', variable= self.radio_var, value= 1)
+        self.rb2 = tkinter.Radiobutton(self.third_frame, text = 'Thick ($1.99)', variable= self.radio_var, value= 2)
+
+        self.rb1.select()
+        #pack radio buttons
+        self.crust.pack(side='top')
+        self.rb1.pack()
+        self.rb2.pack()
+        
+
+
         #pack frames
         self.first_frame.pack(side='top', anchor=tkinter.NW)
         
         self.second_frame.pack()
-        '''
+        
         self.third_frame.pack(side='top')
-        self.forth_frame.pack(side='top')
-        self.fifth_frame.pack(side='top')
-        '''
+ 
         #pack Labels
         self.name.pack(side='left')
         
@@ -57,28 +75,35 @@ class PizzaGUI:
         self.entry_name.pack(side='top')
         
         #main buttons
-        self.paybutton = tkinter.Button(self.main_window, text = 'Pay', command = self.Cal_Price)
+        self.paybutton = tkinter.Button(self.main_window, text = 'Pay', command = self.Calc_Price, font = 'Impact')
         self.quitbutton = tkinter.Button(self.main_window, text = 'Quit', command = self.main_window.destroy)
 
         #pack main buttons
-        self.paybutton.pack(side='bottom')
         self.quitbutton.pack(side='bottom')
+        self.paybutton.pack(side='bottom')
+        
 
 
         tkinter.mainloop()
         
 
-    def Cal_Price(self):
+    def Calc_Price(self):
+        name = self.entry_name.get()
         
-        total = 0
-        if self.cb_pep_var == 1:
-            total += 1
-        if self.cb_pineapple_var == 1:
-            total += 1.5
-        if self.cb_onion_var == 1:
-            total += .5
+        self.total = 0
         
-        self.message =  "\nYour total is: " + str(total)
+        if self.cb_pep_var.get() == 1:
+            self.total += 1
+        if self.cb_pineapple_var.get() == 1:
+            self.total += 1.5
+        if self.cb_onion_var.get() == 1:
+            self.total += .5
+
+        if self.radio_var.get() == 2:
+            self.total += 1.99
+        
+        
+        self.message =  name + "\nYour total is: $" + str(format(self.total, '.2f'))
         tkinter.messagebox.showinfo('Receipt', self.message)
         
 
